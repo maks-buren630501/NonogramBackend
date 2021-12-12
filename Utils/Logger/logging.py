5abcac8375eca3ab5ac858152f4a256116da47e3
evgestrogan <evgestrogan@gmail.com>
2021-12-12 17:12:17 +0300

    [LOGGING] - Created function to initialize logger.

--- Utils/Logger/logging.py
+++ Utils/Logger/logging.py
@@ -1,17 +1,21 @@
 import os
 import logging
-from Utils.Core.Settings import LOGGING
+from Utils.Core.settings import LOGGING
 
 
 if not os.path.exists(LOGGING['DIRECTORY']):
     os.mkdir(LOGGING['DIRECTORY'])
 
-logger = logging.getLogger()
-logger.setLevel(LOGGING["LEVEL"])
 
-handler = logging.FileHandler(filename=f"{LOGGING['DIRECTORY']}/{LOGGING['FILE_NAME']}.log", mode='a+')
-handler.setLevel(LOGGING["LEVEL_FILE"])
-formatter = logging.Formatter(LOGGING['CONTENT_FORMAT'], datefmt=LOGGING['DATE_FORMAT'])
-handler.setFormatter(formatter)
+def get_request_handler():
+    file_handler = logging.FileHandler(filename=f"{LOGGING['DIRECTORY']}/{LOGGING['FILE_NAME']}.log", mode='a+')
+    file_handler.setLevel(LOGGING["LEVEL_FILE"])
+    file_handler.setFormatter(logging.Formatter(LOGGING['CONTENT_FORMAT'], datefmt=LOGGING['DATE_FORMAT']))
+    return file_handler
 
-logger.addHandler(handler)
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(LOGGING["LEVEL"])
+    logger.addHandler(get_request_handler())
+    return logger
--- Utils/Logger/middleware.py
+++ Utils/Logger/middleware.py
@@ -1,8 +1,9 @@
-from Utils.Logger.logging import logger
-
+from fastapi import HTTPException
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
 from starlette.responses import Response
+
+from Utils.Logger.logging import get_logger
 
 
 class LoggingMiddleware(BaseHTTPMiddleware):
@@ -10,8 +11,14 @@ class LoggingMiddleware(BaseHTTPMiddleware):
 
     def __init__(self, app):
         super().__init__(app)
+        self.logger = get_logger(__name__)
 
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
-        response = await call_next(request)
-        logger.info(f"{request.method} {request.client} | {response.status_code}")
-        return response
+        try:
+            response = await call_next(request)
+            self.logger.info(f"{request.method} {request.client} | {response.status_code}")
+            return response
+        except Exception as error:
+            self.logger.info(error, exc_info=True)
+            raise HTTPException(status_code=500, detail='Fatal error')
+
