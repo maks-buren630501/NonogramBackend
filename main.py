137fcec828c12a255ea2ed392fa4413087264304
evgestrogan <evgestrogan@gmail.com>
2021-12-13 17:34:31 +0300

    [TESTS] - created test module.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from Utils.Logger.middleware import LoggingMiddleware
 
@@ -14,3 +14,11 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/{dividend}/{divisor}")
+async def division(dividend: int, divisor: int):
+    try:
+        return {"result": f"{round(dividend / divisor, 2)}"}
+    except ZeroDivisionError:
+        raise HTTPException(detail="ZeroDivisionError", status_code=500)
